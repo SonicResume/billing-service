1431e142cd9290190ac2579bdb0b5cb6c7102455
SonicResume <lubeliafrag@gmail.com>
2026-06-24 19:33:51 -0400

    remove duplicate firebase initialization

--- firebase.py
+++ firebase.py
@@ -1,7 +1,17 @@
+import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase.json")
+firebase_b64 = os.getenv("FIREBASE_KEY_B64")
+
+if not firebase_b64:
+    raise Exception("FIREBASE_KEY_B64 missing")
+
+firebase_json = json.loads(base64.b64decode(firebase_b64))
+
+cred = credentials.Certificate(firebase_json)
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
