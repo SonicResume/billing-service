17ac65e23ae55278229c5baa6c8127e3cc4d723a
SonicResume <lubeliafrag@gmail.com>
2026-06-24 19:38:55 -0400

    cleanup firebase initialization

--- main.py
+++ main.py
@@ -19,23 +19,6 @@ stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 
 # -----------------------------
-# FIREBASE INIT (PRODUCTION SAFE)
-# -----------------------------
-firebase_b64 = os.getenv("FIREBASE_KEY_B64")
-
-if not firebase_b64:
-    raise Exception("FIREBASE_KEY_B64 missing")
-
-firebase_json = json.loads(base64.b64decode(firebase_b64))
-
-cred = credentials.Certificate(firebase_json)
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-# -----------------------------
 # PRICE MAP
 # -----------------------------
 PRICE_MAP = {
