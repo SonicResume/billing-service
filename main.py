fcdeff94f07b35cdff0265fc4e1ec45d09b47928
SonicResume <lubeliafrag@gmail.com>
2026-07-09 09:21:15 -0400

    Fix CORS

--- main.py
+++ main.py
@@ -19,10 +19,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-        "https://noah-language-dkg5mlcr1-sonic-resume.vercel.app",
-        "https://noah-language.vercel.app",
-        "http://localhost:5173",
-    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
